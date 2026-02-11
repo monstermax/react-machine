e9de43ab3e8b004a2a2a0d81eec6dc7c64c78803
Max YO <max@monstermax.net>
2026-02-11 02:37:45 +0100

    dev 2026-02

---
+++ package/src/v3/compiler/arch_custom.ts
@@ -0,0 +1,257 @@
+
+import { Opcode } from '../assembly/core/cpu_instructions';
+
+import type { CPUArchitecture, RegisterDef, InstructionDef } from '@/v2/cpus/default/compiler_v2/compiler.types';
+
+
+const registers: RegisterDef[] = [
+    { name: 'A', aliases: ['AL', 'AH', 'AX', 'EAX', 'RAX'], id: 'A', size: 8 },
+    { name: 'B', aliases: ['BL', 'BH', 'BX', 'EBX', 'RBX'], id: 'B', size: 8 },
+    { name: 'C', aliases: ['CL', 'CH', 'CX', 'ECX', 'RCX'], id: 'C', size: 8 },
+    { name: 'D', aliases: ['DL', 'DH', 'DX', 'EDX', 'RDX'], id: 'D', size: 8 },
+    //{ name: 'SI', aliases: ['ESI', 'RSI'], id: 'SI', size: 8 }, // Source Index (pointeur source)
+    //{ name: 'DI', aliases: ['EDI', 'RDI'], id: 'DI', size: 8 }, // Destination Index (pointeur destination)
+    { name: 'SP', aliases: ['ESP', 'RSP'], id: 'SP', size: 8 },
+    //{ name: 'BP', aliases: ['EBP', 'RBP'], id: 'BP', size: 8 }, // Base Pointer (pointeur de base pour la pile)
+];
+
+
+const instructions: InstructionDef[] = [
+    // CONTROL
+    { mnemonic: 'NOP', opcode: Opcode.NOP, operands: 'NONE', size: 1 },
+    { mnemonic: 'HALT', opcode: Opcode.HALT, operands: 'NONE', size: 1 },
+    { mnemonic: 'HLT', opcode: Opcode.HALT, operands: 'NONE', size: 1 },
+
+    // STACK
+    {
+        mnemonic: 'PUSH', opcode: 0x00, operands: 'NONE', size: 1, variants: [
+            { operands: 'REG', opcode: Opcode.PUSH_REG, size: 2, condition: (ops) => true, mnemonic: 'PUSH_REG' },
+        ]
+    },
+
+    {
+        mnemonic: 'POP', opcode: 0x00, operands: 'NONE', size: 1, variants: [
+            { operands: 'REG', opcode: Opcode.POP_REG, size: 2, condition: (ops) => true, mnemonic: 'POP_REG' },
+        ]
+    },
+
+    {
+        mnemonic: 'CALL', opcode: 0x00, operands: 'NONE', size: 1, variants: [
+            { operands: 'IMM16', opcode: Opcode.CALL, size: 3, condition: (ops) => true, mnemonic: 'CALL' },
+            { operands: 'MEM', opcode: Opcode.CALL, size: 3, condition: (ops) => ops[0].type === 'LABEL', mnemonic: 'CALL' },
+            { operands: 'MEM', opcode: Opcode.CALL, size: 3, condition: (ops) => ops[0].type === 'MEMORY', mnemonic: 'CALL' },
+        ],
+    },
+    { mnemonic: 'RET', opcode: Opcode.RET, operands: 'NONE', size: 1 },
+
+    // INTERRUPTS
+    //{ mnemonic: 'EI', opcode: Opcode.EI, operands: 'NONE', size: 1 },
+    //{ mnemonic: 'DI', opcode: Opcode.DI, operands: 'NONE', size: 1 },
+    //{ mnemonic: 'IRET', opcode: Opcode.IRET, operands: 'NONE', size: 1 },
+
+    // SAUTS
+    {
+        mnemonic: 'JMP', opcode: 0x00, operands: 'NONE', size: 1, variants: [
+            { operands: 'IMM16', opcode: Opcode.JMP, size: 3, condition: (ops) => true, mnemonic: 'JMP' },
+            { operands: 'MEM', opcode: Opcode.JMP, size: 3, condition: (ops) => true, mnemonic: 'JMP' },
+        ],
+    },
+    {
+        mnemonic: 'JZ', opcode: 0x00, operands: 'NONE', size: 1, variants: [
+            { operands: 'IMM16', opcode: Opcode.JZ, size: 3, condition: (ops) => true, mnemonic: 'JZ' },
+            { operands: 'MEM', opcode: Opcode.JZ, size: 3, condition: (ops) => true, mnemonic: 'JZ' },
+        ],
+    },
+    {
+        mnemonic: 'JE', opcode: 0x00, operands: 'NONE', size: 1, variants: [
+            { operands: 'IMM16', opcode: Opcode.JE, size: 3, condition: (ops) => true, mnemonic: 'JE' },
+            { operands: 'MEM', opcode: Opcode.JE, size: 3, condition: (ops) => true, mnemonic: 'JE' },
+        ],
+    },
+    {
+        mnemonic: 'JNZ', opcode: 0x00, operands: 'NONE', size: 1, variants: [
+            { operands: 'IMM16', opcode: Opcode.JNZ, size: 3, condition: (ops) => true, mnemonic: 'JNZ' },
+            { operands: 'MEM', opcode: Opcode.JNZ, size: 3, condition: (ops) => true, mnemonic: 'JNZ' },
+        ],
+    },
+    {
+        mnemonic: 'JNE', opcode: 0x00, operands: 'NONE', size: 1, variants: [
+            { operands: 'IMM16', opcode: Opcode.JNE, size: 3, condition: (ops) => true, mnemonic: 'JNE' },
+            { operands: 'MEM', opcode: Opcode.JNE, size: 3, condition: (ops) => true, mnemonic: 'JNE' },
+        ],
+    },
+    {
+        mnemonic: 'JC', opcode: 0x00, operands: 'NONE', size: 1, variants: [
+            { operands: 'IMM16', opcode: Opcode.JC, size: 3, condition: (ops) => true, mnemonic: 'JC' },
+            { operands: 'MEM', opcode: Opcode.JC, size: 3, condition: (ops) => true, mnemonic: 'JC' },
+        ],
+    },
+    {
+        mnemonic: 'JNC', opcode: 0x00, operands: 'NONE', size: 1, variants: [
+            { operands: 'IMM16', opcode: Opcode.JNC, size: 3, condition: (ops) => true, mnemonic: 'JNC' },
+            { operands: 'MEM', opcode: Opcode.JNC, size: 3, condition: (ops) => true, mnemonic: 'JNC' },
+        ],
+    },
+    {
+        mnemonic: 'JL', opcode: 0x00, operands: 'NONE', size: 1, variants: [
+            { operands: 'IMM16', opcode: Opcode.JL, size: 3, condition: (ops) => true, mnemonic: 'JL' },
+            { operands: 'MEM', opcode: Opcode.JL, size: 3, condition: (ops) => true, mnemonic: 'JL' },
+        ],
+    },
+    {
+        mnemonic: 'JB', opcode: 0x00, operands: 'NONE', size: 1, variants: [
+            { operands: 'IMM16', opcode: Opcode.JB, size: 3, condition: (ops) => true, mnemonic: 'JB' },
+            { operands: 'MEM', opcode: Opcode.JB, size: 3, condition: (ops) => true, mnemonic: 'JB' },
+        ],
+    },
+    {
+        mnemonic: 'JLE', opcode: 0x00, operands: 'NONE', size: 1, variants: [
+            { operands: 'IMM16', opcode: Opcode.JLE, size: 3, condition: (ops) => true, mnemonic: 'JLE' },
+            { operands: 'MEM', opcode: Opcode.JLE, size: 3, condition: (ops) => true, mnemonic: 'JLE' },
+        ],
+    },
+    {
+        mnemonic: 'JBE', opcode: 0x00, operands: 'NONE', size: 1, variants: [
+            { operands: 'IMM16', opcode: Opcode.JBE, size: 3, condition: (ops) => true, mnemonic: 'JBE' },
+            { operands: 'MEM', opcode: Opcode.JBE, size: 3, condition: (ops) => true, mnemonic: 'JBE' },
+        ],
+    },
+    {
+        mnemonic: 'JG', opcode: 0x00, operands: 'NONE', size: 1, variants: [
+            { operands: 'IMM16', opcode: Opcode.JG, size: 3, condition: (ops) => true, mnemonic: 'JG' },
+            { operands: 'MEM', opcode: Opcode.JG, size: 3, condition: (ops) => true, mnemonic: 'JG' },
+        ],
+    },
+    {
+        mnemonic: 'JA', opcode: 0x00, operands: 'NONE', size: 1, variants: [
+            { operands: 'IMM16', opcode: Opcode.JA, size: 3, condition: (ops) => true, mnemonic: 'JA' },
+            { operands: 'MEM', opcode: Opcode.JA, size: 3, condition: (ops) => true, mnemonic: 'JA' },
+        ],
+    },
+    {
+        mnemonic: 'JGE', opcode: 0x00, operands: 'NONE', size: 1, variants: [
+            { operands: 'IMM16', opcode: Opcode.JGE, size: 3, condition: (ops) => true, mnemonic: 'JGE' },
+            { operands: 'MEM', opcode: Opcode.JGE, size: 3, condition: (ops) => true, mnemonic: 'JGE' },
+        ],
+    },
+    {
+        mnemonic: 'JAE', opcode: 0x00, operands: 'NONE', size: 1, variants: [
+            { operands: 'IMM16', opcode: Opcode.JAE, size: 3, condition: (ops) => true, mnemonic: 'JAE' },
+            { operands: 'MEM', opcode: Opcode.JAE, size: 3, condition: (ops) => true, mnemonic: 'JAE' },
+        ],
+    },
+
+    // MOV
+    {
+        mnemonic: 'MOV', opcode: 0x00, operands: 'NONE', size: 1, variants: [
+            { operands: 'REG_IMM16', opcode: Opcode.SET_SP, size: 3, condition: (ops) => ops[0].register === 'SP', mnemonic: 'SET_SP' },
+            { operands: 'REG_MEM', opcode: Opcode.SET_SP, size: 3, condition: (ops) => ops[0].register === 'SP', mnemonic: 'SET_SP' },
+
+            { operands: 'REG_IMM8', opcode: Opcode.MOV_REG_IMM, size: 3, condition: (ops) => true, mnemonic: 'MOV_REG_IMM' },
+            { operands: 'REG_REG', opcode: Opcode.MOV_REG_REG, size: 3, condition: (ops) => true, mnemonic: 'MOV_REG_REG' },
+            { operands: 'REG_MEM', opcode: Opcode.MOV_REG_MEM, size: 4, condition: (ops) => true, mnemonic: 'MOV_REG_MEM' },
+
+            { operands: 'MEM_REG', opcode: Opcode.MOV_MEM_REG, size: 4, condition: (ops) => true, mnemonic: 'MOV_MEM_REG' },
+            { operands: 'MEM_IMM8', opcode: Opcode.MOV_MEM_IMM, size: 4, condition: (ops) => true, mnemonic: 'MOV_MEM_IMM' },
+        ]
+    },
+
+    {
+        mnemonic: 'LEA', opcode: 0x00, operands: 'NONE', size: 1, variants: [
+            { operands: 'REG_REG_IMM16', opcode: Opcode.LEA_REG_REG_IMM, size: 5, condition: (ops) => true, mnemonic: 'LEA_REG_REG_IMM' },
+            { operands: 'REG_REG_MEM', opcode: Opcode.LEA_REG_REG_MEM, size: 5, condition: (ops) => true, mnemonic: 'LEA_REG_REG_MEM' },
+            { operands: 'REG_REG_REG', opcode: Opcode.LEA_REG_REG_REG, size: 4, condition: (ops) => true, mnemonic: 'LEA_REG_REG_REG' },
+        ]
+    },
+
+    // ALU
+    {
+        mnemonic: 'NOT', opcode: 0x00, operands: 'NONE', size: 1, variants: [
+            { operands: 'REG', opcode: Opcode.NOT_REG, size: 2, condition: (ops) => true, mnemonic: 'NOT_REG' },
+        ]
+    },
+
+    {
+        mnemonic: 'INC', opcode: 0x00, operands: 'NONE', size: 1, variants: [
+            { operands: 'REG', opcode: Opcode.INC_REG, size: 2, condition: (ops) => true, mnemonic: 'INC_REG' },
+        ]
+    },
+
+    {
+        mnemonic: 'DEC', opcode: 0x00, operands: 'NONE', size: 1, variants: [
+            { operands: 'REG', opcode: Opcode.DEC_REG, size: 2, condition: (ops) => true, mnemonic: 'DEC_REG' },
+        ]
+    },
+
+    {
+        mnemonic: 'ADD', opcode: 0x00, operands: 'NONE', size: 1, variants: [
+            { operands: 'REG_IMM8', opcode: Opcode.ADD_REG_IMM, size: 3, condition: (ops) => true, mnemonic: 'ADD_REG_IMM' },
+            { operands: 'REG_REG', opcode: Opcode.ADD_REG_REG, size: 3, condition: (ops) => true, mnemonic: 'ADD_REG_REG' },
+        ],
+    },
+
+    {
+        mnemonic: 'SUB', opcode: 0x00, operands: 'NONE', size: 1, variants: [
+            { operands: 'REG_IMM8', opcode: Opcode.SUB_REG_IMM, size: 3, condition: (ops) => true, mnemonic: 'SUB_REG_IMM' },
+            { operands: 'REG_REG', opcode: Opcode.SUB_REG_REG, size: 3, condition: (ops) => true, mnemonic: 'SUB_REG_REG' },
+        ],
+    },
+
+    {
+        mnemonic: 'AND', opcode: 0x00, operands: 'NONE', size: 1, variants: [
+            { operands: 'REG_IMM8', opcode: Opcode.AND_REG_IMM, size: 3, condition: (ops) => true, mnemonic: 'AND_REG_IMM' },
+            { operands: 'REG_REG', opcode: Opcode.AND_REG_REG, size: 3, condition: (ops) => true, mnemonic: 'AND_REG_REG' },
+        ],
+    },
+
+    {
+        mnemonic: 'OR', opcode: 0x00, operands: 'NONE', size: 1, variants: [
+            { operands: 'REG_IMM8', opcode: Opcode.OR_REG_IMM, size: 3, condition: (ops) => true, mnemonic: 'OR_REG_IMM' },
+            { operands: 'REG_REG', opcode: Opcode.OR_REG_REG, size: 3, condition: (ops) => true, mnemonic: 'OR_REG_REG' },
+        ],
+    },
+
+    {
+        mnemonic: 'XOR', opcode: 0x00, operands: 'NONE', size: 1, variants: [
+            { operands: 'REG_IMM8', opcode: Opcode.XOR_REG_IMM, size: 3, condition: (ops) => true, mnemonic: 'XOR_REG_IMM' },
+            { operands: 'REG_REG', opcode: Opcode.XOR_REG_REG, size: 3, condition: (ops) => true, mnemonic: 'XOR_REG_REG' },
+        ],
+    },
+
+    {
+        mnemonic: 'TEST', opcode: 0x00, operands: 'NONE', size: 1, variants: [
+            { operands: 'REG_REG', opcode: Opcode.TEST_REG_REG, size: 3, condition: (ops) => true, mnemonic: 'TEST_REG_REG' },
+            { operands: 'REG_IMM8', opcode: Opcode.TEST_REG_IMM, size: 3, condition: (ops) => true, mnemonic: 'TEST_REG_IMM' },
+        ]
+    },
+
+    {
+        mnemonic: 'CMP', opcode: 0x00, operands: 'NONE', size: 1, variants: [
+            { operands: 'REG_REG', opcode: Opcode.CMP_REG_REG, size: 3, condition: (ops) => true, mnemonic: 'CMP_REG_REG' },
+            { operands: 'REG_IMM8', opcode: Opcode.CMP_REG_IMM, size: 3, condition: (ops) => true, mnemonic: 'CMP_REG_IMM' },
+        ]
+    },
+
+    {
+        mnemonic: 'SHL', opcode: 0x00, operands: 'NONE', size: 1, variants: [
+            { operands: 'REG_IMM8', opcode: Opcode.SHL_REG_IMM, size: 3, condition: (ops) => true, mnemonic: 'SHL_REG_IMM' },
+            { operands: 'REG_REG', opcode: Opcode.SHL_REG_REG, size: 3, condition: (ops) => true, mnemonic: 'SHL_REG_REG' },
+        ]
+    },
+
+    {
+        mnemonic: 'SHR', opcode: 0x00, operands: 'NONE', size: 1, variants: [
+            { operands: 'REG_IMM8', opcode: Opcode.SHR_REG_IMM, size: 3, condition: (ops) => true, mnemonic: 'SHR_REG_IMM' },
+            { operands: 'REG_REG', opcode: Opcode.SHR_REG_REG, size: 3, condition: (ops) => true, mnemonic: 'SHR_REG_REG' },
+        ]
+    },
+];
+
+
+export const CUSTOM_CPU: CPUArchitecture = {
+    name: 'Custom8bit',
+    addressSize: 16,
+    registers,
+    instructions,
+    endianness: 'little',
+};
---
+++ package/src/v3/compiler/compile_test.ts
@@ -0,0 +1,186 @@
+
+import { compileCode, getBytecodeArray, loadSourceCodeFromFile, setCompilationAsmBaseUrl } from '@/v2/lib/compilation';
+import { CUSTOM_CPU } from './arch_custom';
+
+
+async function main() {
+    //console.log('START')
+    await new Promise(r => setTimeout(r, 2000))
+
+    //setCompilationAsmBaseUrl('http://localhost:3938');
+    setCompilationAsmBaseUrl('http://localhost:5173');
+
+    //const sourceCode = await loadSourceCodeFromFile("bootloader/bootloader_v2.asm")
+
+    const sourceCode = `
+
+    ; Author: yomax
+; Date: 2026-01
+; Name: bootloader_v2
+; Description: OS for React Machine (v2)
+
+
+.include "bootloader/bootloader_v2.lib.asm"
+
+
+section .data
+    ; bootloader config
+    BOOTLOADER_VERSION  equ 2
+    INITIAL_FREQ        equ 10
+    SKIP_PRINT_INFO     equ 0x01
+    SKIP_PRINT_RUN      equ 0x01
+
+    ; drivers constants
+    LEDS_STATE_ALL_OFF  equ 0x00
+    LEDS_STATE_HALF_1   equ 0x55
+    LEDS_STATE_HALF_2   equ 0xAA
+
+    ; Variables (références à MEMORY_MAP par exemple)
+    LEDS_BASE    equ 0xF030
+    CLOCK_FREQ   equ 0xF120
+    OS_START     equ 0x0500
+    STACK_END    equ 0xEE0F
+
+    OS_DISK            equ 0x00 ; io num
+    OS_DISK_DATA       equ 0xF000
+    OS_DISK_SIZE_LOW   equ 0xF001
+    OS_DISK_SIZE_HIGH  equ 0xF002
+    OS_DISK_ADDR_LOW   equ 0xF003
+    OS_DISK_ADDR_HIGH  equ 0xF004
+
+    DMA_IO equ 0xF110
+    DMA_ADDR_START_LOW equ 0xF111
+    DMA_ADDR_START_HIGH equ 0xF112
+    DMA_ADDR_END_LOW equ 0xF113
+    DMA_ADDR_END_HIGH equ 0xF114
+    DMA_TARGET_ADDR_LOW equ 0xF115
+    DMA_TARGET_ADDR_HIGH equ 0xF116
+    DMA_DATA           equ 0xF117
+
+
+section .text
+    global INIT
+
+
+; TODO: "Booting from hardisk..." + Load OS in RAM (implement DMA IO read-write)
+
+
+INIT:
+
+MAIN:
+    mov dl, BOOTLOADER_VERSION
+    mov esp, STACK_END ; set stack pointer
+
+    mov al, INITIAL_FREQ
+    mov [CLOCK_FREQ], al ; set clock frequency
+
+RESET_LEDS:
+    nop
+    mov bl, LEDS_STATE_ALL_OFF
+    mov [LEDS_BASE], bl ; eteint les LEDS
+
+    mov al, SKIP_PRINT_INFO
+    cmp al, 1
+    je BOOTLOADER_READY ; skip PRINT_INFO
+
+    call PRINT_INFO ; call PRINT_INFO
+
+BOOTLOADER_READY:
+    inc bl ; label BOOTLOADER_READY
+
+WAIT_FOR_OS:
+    mov [LEDS_BASE], bl ; allume les leds (selon la valeur de B)
+
+    ; double la valeur de B (décalage de bits) ; decale la led à afficher
+    mov al, bl
+    add al, bl
+
+    test al
+    jz BOOTLOADER_READY ; apres l'affichage de la derniere LED, jump to BOOTLOADER_READY
+
+    mov bl, al
+
+    ; check OS on disk
+    mov [OS_DISK_ADDR_LOW], 0
+    mov [OS_DISK_ADDR_HIGH], 0
+    mov al, [OS_DISK_DATA]
+    test al
+    jz WAIT_FOR_OS ; si pas d'OS chargé on retourne à WAIT_FOR_OS
+    call LOAD_OS_IN_RAM
+
+    ; check OS in RAM
+    mov al, [OS_START] ; detection de chargement de l'OS
+    test al
+    jz WAIT_FOR_OS ; si pas d'OS chargé on retourne à WAIT_FOR_OS
+
+    ; un OS a été trouvé. on se prépare à le lancer
+
+    call RUN_OS
+
+
+LOAD_OS_IN_RAM:
+    mov [DMA_IO], OS_DISK ; define IO (OS_DISK=device0)
+
+    mov [DMA_ADDR_START_LOW] , 0x00 ; define start disk address (low byte)
+    mov [DMA_ADDR_START_HIGH], 0x00 ; define start disk address (high byte)
+
+    mov al, [OS_DISK_SIZE_LOW]  ; load disk size (low)
+    mov bl, [OS_DISK_SIZE_HIGH] ; load disk size (high)
+
+    dec al
+    jnc LOAD_OS_IN_RAM_NO_CARRY
+    dec bl
+
+    LOAD_OS_IN_RAM_NO_CARRY:
+    mov [DMA_ADDR_END_LOW] , al ; define end disk address (low byte) ;; TODO: -1
+    mov [DMA_ADDR_END_HIGH], bl ; define end disk address (high byte) ;; TODO: -1
+
+    mov [DMA_TARGET_ADDR_LOW] , 0x00 ; define target ram address (low byte)
+    mov [DMA_TARGET_ADDR_HIGH], 0x05 ; define target ram address (high byte)
+
+    mov [DMA_DATA], 1
+    ret
+
+
+RUN_OS:
+    mov al, SKIP_PRINT_RUN
+    cmp al, 1
+    je BOOTLOADER_LEAVE ; skip PRINT_RUN
+
+    call PRINT_RUN ; label RUN_OS. call PRINT_RUN
+
+BOOTLOADER_LEAVE:
+    mov al, 0x00
+    mov [LEDS_BASE], al ; eteint les leds
+
+    ; SET_FREQ 10 (commenté dans votre code original)
+    ; call @OS_START
+    call OS_START ; call OS_START
+
+OS_RETURN:
+    ; JMP $INIT
+    jmp INIT ; os return. jump to INIT
+
+hlt
+
+
+
+    `;
+
+
+    const compiled = await compileCode(sourceCode, CUSTOM_CPU);
+
+    //console.log('compiled:', compiled)
+
+    //const [text, data, bss] = compiled.sections
+    //console.log('text:', text)
+    //console.log('data:', data)
+
+    const code = getBytecodeArray(compiled)
+    console.log('code:', code)
+}
+
+main();
+
+
+
